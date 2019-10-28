b75f2c262287e4ff8994c49f9c554c6b099f5219
Holger Vetter <hvetter@daenet.de>
2019-10-28 20:55:53 +0100

    first working version

---
+++ KMLFlattener/KMLFile.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace KMLFlattener
+{
+
+    // HINWEIS: Für den generierten Code ist möglicherweise mindestens .NET Framework 4.5 oder .NET Core/Standard 2.0 erforderlich.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true/*, Namespace = "http://earth.google.com/kml/2.1"*/)]
+    [System.Xml.Serialization.XmlRootAttribute(/*Namespace = "http://earth.google.com/kml/2.1",*/ IsNullable = false)]
+    public partial class kml
+    {
+
+        private kmlDocument documentField;
+
+        /// <remarks/>
+        public kmlDocument Document
+        {
+            get
+            {
+                return this.documentField;
+            }
+            set
+            {
+                this.documentField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true/*, Namespace = "http://earth.google.com/kml/2.1"*/)]
+    public partial class kmlDocument
+    {
+
+        private string nameField;
+
+        private byte openField;
+
+        private kmlDocumentFolder[] folderField;
+
+        /// <remarks/>
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte open
+        {
+            get
+            {
+                return this.openField;
+            }
+            set
+            {
+                this.openField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Folder")]
+        public kmlDocumentFolder[] Folder
+        {
+            get
+            {
+                return this.folderField;
+            }
+            set
+            {
+                this.folderField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Placemark")]
+        public kmlDocumentFolderPlacemark[] Placemark
+        {
+            get;
+            set;
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true/*, Namespace = "http://earth.google.com/kml/2.1"*/)]
+    public partial class kmlDocumentFolder
+    {
+
+        private string nameField;
+
+        private byte openField;
+
+        private kmlDocumentFolderPlacemark[] placemarkField;
+
+        /// <remarks/>
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte open
+        {
+            get
+            {
+                return this.openField;
+            }
+            set
+            {
+                this.openField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Placemark")]
+        public kmlDocumentFolderPlacemark[] Placemark
+        {
+            get
+            {
+                return this.placemarkField;
+            }
+            set
+            {
+                this.placemarkField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true/*, Namespace = "http://earth.google.com/kml/2.1"*/)]
+    public partial class kmlDocumentFolderPlacemark
+    {
+
+        //private string nameField;
+
+        private kmlDocumentFolderPlacemarkPoint pointField;
+
+        private object snippetField;
+
+        //private string descriptionField;
+
+        /// <remarks/>
+        public XmlCDataSection name
+        {
+            get;
+            //{
+            //    return this.nameField;
+            //}
+            set;
+            //{
+            //    this.nameField = value;
+            //}
+        }
+
+        /// <remarks/>
+        public kmlDocumentFolderPlacemarkPoint Point
+        {
+            get
+            {
+                return this.pointField;
+            }
+            set
+            {
+                this.pointField = value;
+            }
+        }
+
+        /// <remarks/>
+        public object Snippet
+        {
+            get
+            {
+                return this.snippetField;
+            }
+            set
+            {
+                this.snippetField = value;
+            }
+        }
+
+        //XmlCDataSection
+        /// <remarks/>
+        public XmlCDataSection description
+        {
+            get;
+            //{
+            //    return this.descriptionField;
+            //}
+            set;
+            //{
+            //    this.descriptionField = value;
+            //}
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true/*, Namespace = "http://earth.google.com/kml/2.1"*/)]
+    public partial class kmlDocumentFolderPlacemarkPoint
+    {
+
+        private string coordinatesField;
+
+        /// <remarks/>
+        public string coordinates
+        {
+            get
+            {
+                return this.coordinatesField;
+            }
+            set
+            {
+                this.coordinatesField = value;
+            }
+        }
+    }
+
+
+}
---
+++ KMLFlattener/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace KMLFlattener
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            //dlg.FileName = "KML File"; // Default file name
+            //dlg.DefaultExt = ".kml"; // Default file extension
+            //dlg.Filter = "KML (.kml)|*.kml"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                TxtFilePath.Text = dlg.FileName;
+            }
+        }
+
+        private void BtnFlatten_Click(object sender, RoutedEventArgs e)
+        {
+            var filePath = TxtFilePath.Text;
+
+            if (!File.Exists(filePath))
+                MessageBox.Show("Invalid Filepath!");
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(kml));
+
+            kml oldKmlFile;
+
+            using (var stream = File.OpenRead(filePath))
+            {
+                using (XmlTextReader reader = new XmlTextReader(stream))
+                {
+                    reader.Namespaces = false;
+                    oldKmlFile = xmlSerializer.Deserialize(reader) as kml;
+                }
+            }
+
+            if (oldKmlFile == null)
+                MessageBox.Show("Invalid KML file!");
+
+            flattenKml(oldKmlFile);
+
+            string newFileName = "New_" + Path.GetFileNameWithoutExtension(filePath) + ".kml";
+
+            var encoding = Encoding.GetEncoding("UTF-8");
+
+            using (StreamWriter stream = new StreamWriter(newFileName, false, encoding))
+            {
+                xmlSerializer.Serialize(stream, oldKmlFile);
+            }
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), newFileName);
+            MessageBox.Show($"Successful flattened kml file. Path: {path}");
+        }
+
+        private void flattenKml(kml kmlFile)
+        {
+            if (kmlFile.Document.Folder == null)
+                return;
+
+             var flattenedPlacemarks = kmlFile.Document.Folder.SelectMany(d => d.Placemark);
+
+            if (kmlFile.Document.Placemark == null)
+            {
+                kmlFile.Document.Placemark = flattenedPlacemarks.ToArray();
+            }
+            else // merge placemarks
+            {
+                List<kmlDocumentFolderPlacemark> newPlaceMarks = new List<kmlDocumentFolderPlacemark>(kmlFile.Document.Placemark);
+
+                newPlaceMarks.AddRange(flattenedPlacemarks);
+
+                kmlFile.Document.Placemark = newPlaceMarks.ToArray();
+            }
+
+            kmlFile.Document.Folder = null;
+        }
+    }
+}
